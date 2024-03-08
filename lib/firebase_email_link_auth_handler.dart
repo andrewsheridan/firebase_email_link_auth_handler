5c315265032815ad5d36c96cdaa56edc2e162f93
Andrew Sheridan <sheridan.andrew13@gmail.com>
2024-03-07 18:07:35 -0700

    Initial commit.

--- lib/firebase_email_link_auth_handler.dart
+++ lib/firebase_email_link_auth_handler.dart
@@ -1,7 +1,35 @@
-library firebase_email_link_auth_handler;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// Visit https://firebase.google.com/docs/auth/flutter/email-link-auth
+class EmailLinkAuthHandler extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth;
+  final ActionCodeSettings settings;
+  final Logger _logger = Logger("EmailLinkAuthHandler");
+  bool _loginInProgress = false;
+
+  bool get loginInProgress => _loginInProgress;
+
+  EmailLinkAuthHandler({
+    FirebaseAuth? firebaseAuth,
+    required this.settings,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  Future<void> login(String email) async {
+    try {
+      _loginInProgress = true;
+      notifyListeners();
+
+      await _firebaseAuth.sendSignInLinkToEmail(
+        email: email,
+        actionCodeSettings: settings,
+      );
+    } catch (ex) {
+      _logger.severe("Failed to send sign in link to email $email.", ex);
+    } finally {
+      _loginInProgress = false;
+      notifyListeners();
+    }
+  }
 }
